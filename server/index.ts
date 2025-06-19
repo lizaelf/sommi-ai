dee53eb066a1527b3c6e062901df35d7ee83d831
MaksymKonoplinskyi <gran7777777@gmail.com>
2025-06-19 12:52:44 +0300

    fix dev mod

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,9 @@
-import dotenv from "dotenv";
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
