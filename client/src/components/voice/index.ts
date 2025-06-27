90eb2fd9fda5b2f083c43bc4902d1ca6e71daf14
MaksymKonoplinskyi <gran7777777@gmail.com>
2025-06-27 11:25:16 +0300

    remove voice services

--- client/src/components/voice/index.ts
+++ client/src/components/voice/index.ts
@@ -1,4 +1,1 @@
 export { default as VoiceController } from './VoiceController';
-export { default as VoiceStateManager } from './VoiceStateManager';
-export { default as VoiceAudioManager } from './VoiceAudioManager';
-export { default as VoiceRecorder } from './VoiceRecorder';
